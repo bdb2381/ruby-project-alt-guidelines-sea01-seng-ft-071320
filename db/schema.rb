cb6c5592ed3339a677bfe0fe04604cc9aea6658c
bdb2381 <brad.beggs@gmail.com>
2020-07-28 10:13:16 -0700

    Revert "Revert "added password schema and seeds""
    This reverts commit 7e89ecc5807cd03981b061677e385d5a7e5b8037.

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_07_27_215132) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "password"
     t.string "location"
     t.string "race"
     t.string "gender"
