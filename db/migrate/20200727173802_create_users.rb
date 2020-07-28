bd35376534bf5b79e41da02aa073c20279a25d26
dbpatnodee <dbpatnode@hotmail.com>
2020-07-28 09:06:24 -0700

    added password schema and seeds

--- db/migrate/20200727173802_create_users.rb
+++ db/migrate/20200727173802_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :username
+      t.string :password
       t.string :location
       t.string :race
       t.string :gender
