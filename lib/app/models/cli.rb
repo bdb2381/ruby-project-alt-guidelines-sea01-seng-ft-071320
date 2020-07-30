ce36144ee332e9a0a590f069f893d9b42ad16cab
LucasLeibs <dr.lucasleibs@gmail.com>
2020-07-30 11:45:01 -0700

    add full spacing, logout option

--- lib/app/models/cli.rb
+++ lib/app/models/cli.rb
@@ -37,10 +37,14 @@ class CLI
         system "clear"
         puts render_banner
         prompt = TTY::Prompt.new(active_color: :red)
+    
         puts "Please enter your username:".blue.bold
         username = gets.chomp.downcase.titleize
+        puts "\n"
+        
         password = prompt.mask("Enter your password".blue.bold) 
         user_instance = User.find_by(username: username, password: password)
+        
         if user_instance
             puts "Welcome back looks like you already have an acount with us, lets take you to the Main Menu"  
             sleep 2
@@ -73,7 +77,7 @@ class CLI
         user_instance = User.create(username: username, password: password, location: location, race: race, gender: gender)
         
         puts "Thanks for setting up an account." 
-        sleep 2
+        sleep 3
         main_menu(user_instance)
     end
     
@@ -158,11 +162,12 @@ class CLI
         system "clear"
         puts render_banner
         prompt = TTY::Prompt.new(active_color: :red)
-        menu_choice = prompt.select("Please select from the folowing options".blue.bold + " #{user_instance.username}".red.bold, cycle: true, echo: false) do |menu| 
+        menu_choice = prompt.select("Please select from the folowing options".blue.bold + " #{user_instance.username}".red.bold + ":".blue.bold, cycle: true, echo: false) do |menu| 
             menu.choice 'Rate an officer',-> {rate_officer(user_instance)}
             menu.choice 'View officer average rating',-> {get_average_rating(user_instance)}
             menu.choice 'Change a previous review',-> {change_rating(user_instance)}
             menu.choice 'Delete previous review',-> {delete_review(user_instance)}
+            menu.choice 'Logout',-> {login_menu}
             menu.choice 'Exit',-> {exit}
         end
     end
@@ -178,21 +183,25 @@ class CLI
         @officer_instance = Officer.find_by(officer_name: officer)
         sleep 1
         
+        puts "\n"
         puts "You chose:" + " #{@officer_instance.officer_name}".red.bold  
-        puts "Please rate your interaction with the Officer.".blue.bold
-        rating = prompt.slider("Rating", max: 10, step: 0.5, default: 0, format: "|:slider| %.1f")
+        puts "Please rate your interaction with the Officer:".blue.bold
         
+        puts "\n"
+        rating = prompt.slider("Rating", max: 10, step: 0.5, default: 0, format: "|:slider| %.1f")
+        puts "\n"
         puts "Please include a description of your interaction with".blue.bold + " #{@officer_instance.officer_name}".red.bold + ":".blue.bold
         review_desc = gets.chomp 
-
+        
+        puts "\n"
         puts "Are you sure you want to submit this review?".blue.bold
         are_you_sure(user_instance)
 
         @review_instance = Review.create(user: user_instance, officer: @officer_instance, rating: rating, review_desc: review_desc)
         sleep 1
-        
+        puts "\n"
         puts "Thank you" + " #{user_instance.username}".red.bold + " for your review of" + " #{@officer_instance.rank}".red.bold + " #{@officer_instance.officer_name}".red.bold + "."
-        puts "Who works in" + " #{@officer_instance.preicient}".red.bold + ", badge number" + " #{@officer_instance.badge_number}".red.bold 
+        puts "Who works out of the" + " #{@officer_instance.preicient}".red.bold + ", badge number" + " #{@officer_instance.badge_number}".red.bold 
         puts "You have given them a review of:" + " #{rating}/10".red.bold + " and a description of:" + " #{review_desc}".red.bold
         
         go_back_or_repeat("Do you want to submit another rating?", user_instance)
@@ -226,10 +235,11 @@ class CLI
         
         officer_choices = get_officer_instance
         officer = prompt.select("Choose an officer to veiw their average rating:".blue.bold, officer_choices, cycle: true , echo: false, filter: true)
+        puts "\n"
         
         @officer_instance = Officer.find_by(officer_name: officer)
         officer_review_selection = Review.all.select {|review| review.officer_id == @officer_instance.id}
-
+        
         total_reviews = officer_review_selection.map {|review| review.rating}
         avg_rating = total_reviews.sum / total_reviews.length
 
@@ -259,18 +269,23 @@ class CLI
             
             officer_review = reviews.select {|review| review.officer.officer_name == officer_selection}
             officer_x = Officer.find_by(officer_name: officer_selection)
-
+            puts "\n"
+            
             puts "Please update your rating for:".blue.bold + " #{officer_selection}".red.bold
+            puts "\n"
             rating = prompt.slider("Rating", max: 10, step: 0.5, default: 0, format: "|:slider| %.1f")
             sleep 1
+            puts "\n"
             
             puts "Please include a description of your interaction with:".blue.bold + " #{officer_x.rank} #{officer_x.officer_name}".red.bold
             review_desc = gets.chomp
-            
+            puts "\n"
+        
             review_id = officer_review.map {|review| review.id}
 
             puts "Are you sure you want to submit this updated review?".blue.bold
             are_you_sure(user_instance)
+            puts "\n"
             
             Review.update(review_id[0], rating: rating, review_desc: review_desc)
             
@@ -296,17 +311,19 @@ class CLI
             reviews = Review.all.select {|review| review.user_id == user_instance.id} #selecting all reviews of officers that match reviews by user
             officer_names = reviews.map {|review| review.officer.officer_name} #return names as array of strings
             officer =  prompt.select("Please select a review to delete:".blue.bold, officer_names, cycle: true , echo: false, filter: true)#tty menu
-            
+            puts "\n"
             
             officer_review = reviews.select {|review| review.officer.officer_name == officer} #returns review instance associated with the name entered above
             officer_x = Officer.find_by(officer_name: officer) #matching officer instance
             review_id = officer_review.map {|review| review.id} #finds id for review instance
-            
+            puts "\n"
+        
             puts "Are you sure you want to delete this review?".blue.bold
             are_you_sure(user_instance)
-
+            puts "\n"
+            
             Review.destroy(review_id[0]) #deletes review.    
-            puts "You have deleted your rating of:" " #{officer}".red.bold
+            puts "You have deleted your rating of:".bold + " #{officer}".red.bold
         else
             puts "You do not have any reviews to delete. Lets take you back to the main menu."
         end
